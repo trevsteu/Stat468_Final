a12bd70991bf483732436c137fffb5ee60a6b023
Trevor Steunenberg <tsteu35@gmail.com>
2025-07-16 19:40:57 -0400

    added functions.R

---
+++ functions.R
@@ -0,0 +1,35 @@
+library(rvest)
+library(tidyverse)
+library(janitor)
+
+start_year <- 1996
+end_year <- 2020
+
+
+import_draft <- function(year){
+  url <- str_glue("https://www.hockey-reference.com/draft/NHL_{year}_entry.html")
+  html <- read_html(url)
+  Sys.sleep(10) # to avoid getting rate limited
+  draft_year_table <- html |> 
+    html_element("table") |> 
+    html_table() |> 
+    janitor::row_to_names(1) |> 
+    janitor::clean_names()
+  draft_year_table
+}
+
+tidy_draft <- function(year){
+  draft_year_table <- import_draft(year) |> 
+    filter(overall != "Overall" & overall != "")|> # remove extra rows
+    type_convert() |> # fix types 
+    mutate("year" = year, "ps" = pmax(coalesce(ps, 0), 0), 
+           "gp" = coalesce(gp, 0), "to" = coalesce(to, year)) |> 
+    select(year, overall, to, pos, gp, ps) # columns we care about
+  draft_year_table
+}
+
+all_data <- do.call(rbind, lapply(seq(start_year, end_year, 1),
+                                  \(x) tidy_draft(x)))
+
+
+
