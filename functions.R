c0d1ab5f0f409f4d1d96c6ae61693cbbc352f823
Trevor Steunenberg <tsteu35@gmail.com>
2025-08-01 19:58:20 -0400

    duck db stuff

--- functions.R
+++ functions.R
@@ -5,6 +5,9 @@ library(dplyr)
 library(stringr)
 library(DBI)
 library(duckdb)
+library(aws.s3)
+library(paws)
+library(arrow)
 
 
 start_year <- 1996
@@ -42,81 +45,18 @@ tidy_draft <- function(year){
 
 # ---------------------------------------------------------------------------------------
 
-# This is a little bit circular. In order to create the table in the database we need
-#   to first read the data from a csv (for the sake of rendering time). We then run the  
-#   same code as in the Tidy chapter. 
+# We don't need to add the files to the bucket since they're already there. 
+con <- dbConnect(duckdb())
 
-all_data <- read.csv("all_data.csv") 
+dbExecute(con, "INSTALL httpfs;")
+dbExecute(con, "LOAD httpfs;")
 
-con <- DBI::dbConnect(duckdb::duckdb(), dbdir = "trevor-stat468.duckdb")
-DBI::dbWriteTable(con, "all_data", all_data, overwrite = TRUE)
-
-sql <- 'SELECT * FROM "trevor-stat468"."all_data"'
-all_data <- as.data.frame(dbGetQuery(con, sql))
-DBI::dbDisconnect(con)
-
-# ---------------------------------------------------------------------------------------
-
-get_length <- Vectorize(function(len){ 
-  all_data |> 
-    mutate(rem_career_len = to - year - len) |> 
-    filter(year %in% 1996:2004 & rem_career_len >= 0) |> 
-    summarize(mean = mean(rem_career_len)) |> 
-    pull(mean)
-})
-
-est_yr <- data.frame(k = seq(1,22)) |>
-  mutate(yr = get_length(k))
-
-# ---------------------------------------------------------------------------------------
-
-active_players <- all_data |> 
-  filter(to == 2025) |> 
-  mutate(career_len = to - year) |> 
-  mutate(adj_ps = ps + round(ps / career_len * get_length(career_len), 2)) |> 
-  select(-career_len)
-
-inactive_players <- all_data |> 
-  filter(to != 2025) |> 
-  mutate(adj_ps = ps)
-
-
-all_data_adj <- rbind(active_players, inactive_players)
-
-# ---------------------------------------------------------------------------------------
-
-get_threshold <- function(year){  
-  ifelse(year <= 2017, 200, 82/3*(2025-year)) 
-} 
-
-thresholds <- data.frame(year = seq(2015,2020)) |>    
-  mutate(t = get_threshold(year))  
-
-# ---------------------------------------------------------------------------------------
-
-all_data_ret <- all_data_adj |>    
-  filter(to != 2025) |>   
-  mutate(thresh = ifelse(pos == "G", 100, 200),           
-         reg = gp >= thresh) |>    
-  select(-thresh)  
-
-all_data_act <- all_data_adj |>    
-  filter(to == 2025) |>    
-  mutate(thresh = ifelse(pos == "G", get_threshold(year) / 2,                           
-                         get_threshold(year)),         
-         reg = gp >= thresh) |>    
-  select(-thresh)  
-
-all_data_adj <- rbind(all_data_ret, all_data_act)
-
-# ---------------------------------------------------------------------------------------
-
-all_data_comb <- all_data_adj |> 
-  group_by(overall) |> 
-  summarize(mean_ps = mean(ps), 
-            mean_gp = mean(gp), 
-            mean_adj_ps = mean(adj_ps),
-            suc_rate = mean(reg))
+all_data <- dbGetQuery(con, "SELECT * 
+                            FROM read_parquet('s3://trevor-stat468/all_data.parquet');")
+all_data_adj <- dbGetQuery(con, "SELECT * 
+                            FROM read_parquet('s3://trevor-stat468/all_data_adj.parquet');")
+all_data_comb <- dbGetQuery(con, "SELECT * 
+                            FROM read_parquet('s3://trevor-stat468/all_data_comb.parquet');")
 
 # ---------------------------------------------------------------------------------------
 
